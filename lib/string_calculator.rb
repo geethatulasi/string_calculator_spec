d3ca841352f1b1789d70ff39d6a6bb9cd88be9d7
geethatulasi <geethatulasi007@gmail.com>
2025-06-27 07:21:47 +0000

    raise an error when negative numbers are passed test case passed

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 def add(str_numbers)
 	return 0 if str_numbers.strip.empty?
-	numbers = str_numbers.scan(/\d+/).map(&:to_i).select{ |n| n < 1000 }
+	numbers = str_numbers.scan(/-?\d+/).map(&:to_i).select{ |n| n < 1000 }
+	negatives = numbers.select(&:negative?)
+	raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
 	return numbers.sum
 end
