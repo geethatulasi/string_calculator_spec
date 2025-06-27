6d006b46336d2f621a3ea76de195c5c1961aa15d
geethatulasi <geethatulasi007@gmail.com>
2025-06-27 06:58:04 +0000

    failed to return 1

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,7 @@ RSpec.describe "#add" do
 	it 'return 0 when pass empty string numbers' do
 		expect(add('')).to eq(0)
 	end
+	it 'return 1 when pass "1"' do
+    expect(add('1')).to eq(1)
+  end
 end
