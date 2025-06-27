da4d05aa7ea183ec141e2fe124f67eaf6a10fce6
geethatulasi <geethatulasi007@gmail.com>
2025-06-27 07:12:03 +0000

    return 10 test case for '//|\n1|2|3|4|1001' passed

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,5 @@
 def add(str_numbers)
 	return 0 if str_numbers.strip.empty?
-	numbers = str_numbers.scan(/\d+/).map(&:to_i)
+	numbers = str_numbers.scan(/\d+/).map(&:to_i).select{ |n| n < 1000 }
 	return numbers.sum
 end
